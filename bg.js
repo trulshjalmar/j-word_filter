1b9b89a1937d7f36d8f5b1a02082cb8b262cf324
trulshjalmar <86436012+trulshjalmar@users.noreply.github.com>
2026-05-12 01:07:48 +0200

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,13 +1,15 @@
-// Lytter etter beskjeder fra nettsiden om hvor mange ord som er sensurert
+/**
+ * Background service for badge management
+ */
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "updateBadge" && sender.tab) {
-        // Viser antall ord på ikonet (kun på den spesifikke fanen)
+        
         chrome.action.setBadgeText({
             text: request.count > 0 ? request.count.toString() : "",
             tabId: sender.tab.id
         });
         
-        // Setter fargen på den lille boblen til "varsel-rød"
+        
         chrome.action.setBadgeBackgroundColor({
             color: "#ff4757",
             tabId: sender.tab.id
